dd3917a3246521c4cdab5a71f864e7179f649ca3
swq <604164227@qq.com>
2025-09-16 17:36:24 +0800

    添加请求头以支持微博热搜数据抓取

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ const TRENDING_DETAIL_URL = 'https://m.s.weibo.com/topic/detail?q='
 
 let RETRY_TIME = 5
 
+const headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.69',
+  Cookie: 'SUB=_2AkMflEwGf8NxqwFRmvsXxG7ia4h2wwrEieKpyL3dJRM3HRl-yT9yqk4mtRB6NBRi6maz6YaTyfIClrCyCUrm0-7nB1R9; SUBP=0033WrSXqPxfM72-Ws9jqgMF55529P9D9WhR9EPgz3BDPWy-YHwFuiIb; MLOGIN=0; _T_WM=32026405228; WEIBOCN_FROM=1110006030; XSRF-TOKEN=4f0061; mweibo_short_token=7c696d3a05; M_WEIBOCN_PARAMS=luicode%3D10000011%26lfid%3D106003type%253D25%2526t%253D3%2526disable_hot%253D1%2526filter_type%253Drealtimehot%26fid%3D106003type%253D25%2526t%253D3%2526disable_hot%253D1%2526filter_type%253Drealtimehot%26uicode%3D10000011',
+}
+
 async function saveRawJson(data) {
   const date = format(new TZDate(Date.now(), "Asia/Shanghai"), 'yyyy-MM-dd');
   const apiDir = path.join(__dirname, 'api')
@@ -29,7 +34,7 @@ function getText(str) {
 
 async function fetchTrendingDetail(title) {
   try {
-    const res = await fetch(TRENDING_DETAIL_URL + title)
+    const res = await fetch(TRENDING_DETAIL_URL + title, { headers })
     const data = await res.text()
     const $ = cheerio.load(data)
     return {
@@ -44,7 +49,9 @@ async function fetchTrendingDetail(title) {
 async function bootstrap() {
   while (RETRY_TIME > 0) {
     try {
-      const res = await fetch(TRENDING_URL).then(res => res.json())
+      const res = await fetch(TRENDING_URL, {
+        headers,
+      }).then(res => res.json())
       const list = res.data.cards[0].card_group
         .filter((k) => !k.pic.includes('img_search_stick') && k.desc && !k.actionlog?.ext.includes("ads_word"))
         .map((k) => {
@@ -54,14 +61,14 @@ async function bootstrap() {
             scheme: k.scheme,
           }
         })
-      const detailList = await Promise.all(list.map(c => fetchTrendingDetail(c.desc)))
-      const result = []
-      detailList.forEach((detail, index) => {
-        const item = list[index]
-        item.category = detail.category
-        item.description = detail.description
-        result.push(item)
-      })
+      // const detailList = await Promise.all(list.map(c => fetchTrendingDetail(c.desc)))
+      const result = list
+      // detailList.forEach((detail, index) => {
+      //   const item = list[index]
+      //   item.category = detail.category
+      //   item.description = detail.description
+      //   result.push(item)
+      // })
       await saveRawJson(result)
       RETRY_TIME = 0
     } catch (err) {
